b25f6154214fb00f9a909851ffc5c03185700cb4
Afonsommlourenco <83539918+Afonsommlourenco@users.noreply.github.com>
2021-05-17 23:16:29 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
-
-use App\Http\Controllers\EstampasController;
-use App\Http\Controllers\TshirtsController;
-use App\Http\Controllers\CatalogoController;
-use App\Http\Controllers\CarrinhoController;
-use App\Http\Controllers\EncomendasController;
+use App\Http\Controllers\StampsController;
+use App\Http\Controllers\CatalogueController;
+use App\Http\Controllers\OrdersController;
 
 
 
@@ -27,7 +24,7 @@ use App\Http\Controllers\EncomendasController;
 Route::get('/',[HomeController::class,'index'])->name('Home');
 Route::get('estampas', [StampsController::class, 'index'])->name('Stamps');
 Route::get('catalogo', [CatalogueController::class, 'index'])->name('Catalogue');
-Route::get('carrinho', [CartController::class, 'index'])->name('Cart');
+Route::get('carrinho', [OrdersController::class, 'orders'])->name('Cart');
 Route::get('entrar', [UserController::class, 'index'])->name('Login');
 Route::get('registar', [UserController::class, 'registerPage'])->name('Register');
 
