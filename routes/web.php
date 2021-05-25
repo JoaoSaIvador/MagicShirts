66d5ff0fdc6413564f2120c105cc7e18de42d388
Afonsommlourenco <83539918+Afonsommlourenco@users.noreply.github.com>
2021-05-25 15:52:19 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CatalogueController;
 use App\Http\Controllers\OrdersController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\IndexController;
+use App\Http\Controllers\HomeController;
+
 
 
 
@@ -23,25 +22,18 @@ use App\Http\Controllers\IndexController;
 |
 */
 
-Route::get('entrar', [UserController::class, 'index'])->name('Login');
-Route::get('registar', [UserController::class, 'registerPage'])->name('Register');
 Route::get('/',[HomeController::class,'index'])->name('Home');
+Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('catalogo', [CatalogueController::class, 'index'])->name('Catalogue');
 Route::get('catalogo/produto/{estampa}', [ProductController::class, 'index'])->name('Product.view');
 Route::get('carrinho', [CartController::class, 'index'])->name('Cart');
 
-//Route::post('catalogo', [ProductController::class, 'store'])->name('Product.store');
 Route::post('carrinho', [CartController::class, 'store_tshirt'])->name('Cart.store');
 
 Route::put('carrinho', [CartController::class, 'update_tshirt'])->name('Cart.update');
 Route::delete('carrinho', [CartController::class, 'destroy_tshirt'])->name('Cart.destroy');
 
-Route::post('register', [UserController::class, 'register']);
-
-
 Auth::routes();
-
-
 
 
