102e8bf4e214c4802c7e42d91fd45fa97d5a7408
Afonsommlourenco <83539918+Afonsommlourenco@users.noreply.github.com>
2021-05-26 20:09:38 +0100

    f

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,7 +1,60 @@
 @extends('template')
 @section('content')
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{asset('img/Carrousel2.jpg')}}" class="d-block" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/Carrousel2.jpg')}}" class="d-block" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/Carrousel2.jpg')}}" class="d-block" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 
-<h1>g</h1>
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{asset('img/Carrousel1.jpg')}}" class="d-block w-100" alt="Shirt">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/Carrousel2.jpg')}}" class="d-block w-100" alt="Shirt">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/Carrousel3.jpg')}}" class="d-block w-100" alt="Shirt">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/Carrousel4.jpg')}}" class="d-block w-100" alt="Shirt">
+    </div>
+    <div class="carousel-item">
+      <img src="{{asset('img/Carrousel5.jpg')}}" class="d-block w-100" alt="Shirt">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,10 +7,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{asset('CSS/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{asset('CSS/Stylesheet.css')}}" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{asset('css/stylesheet.css')}}">
-
-    <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
     <title>MagicShirts</title>
    
 </head>
@@ -18,11 +17,8 @@
 <nav class="mainmenu navbar navbar-expand-lg mb-4 static-top shadow">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">
-    <img src="{{asset('img/MagicShirtsLogo.png')}}" alt="MagicShirts LOGO">
+        <img src="{{asset('img/MagicShirtsLogo.png')}}" alt="MagicShirts LOGO">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
