3477c2fd86b6c08735bd1ce0dc19c26f9068678a
Afonsommlourenco <83539918+Afonsommlourenco@users.noreply.github.com>
2021-05-28 12:35:22 +0100

    f

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -45,7 +45,6 @@
                     <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
                 </li>
             @endguest
@@ -55,5 +54,6 @@
   </div>
 </nav>
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
