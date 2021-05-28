00aa5a12fff0e29fa0f43bc6742d7eaa6a3967da
Afonsommlourenco <83539918+Afonsommlourenco@users.noreply.github.com>
2021-05-29 00:37:23 +0100

    f

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,73 +3,72 @@
 @section('content')
 <div class="container">
     <div class="justify-content-center">
-        <div class="authCard">
-            <div class="authCardBody">
-                <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <div class="authCardBody">
+            <form method="POST" action="{{ route('login') }}">@csrf
 
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label">{{ __('Endereço E-Mail') }}</label>
-                        <div>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <div class="authLogo">
+                    <img src="{{asset('img/MagicShirtsLogo.png')}}" alt="MagicShirts LOGO">
+                </div>
 
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                <div class="form-group row">
+                    <label for="email">{{ __('Endereço E-Mail') }}</label>
+                    <div>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password">{{ __('Password') }}</label>
+                    <div>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-md-6 ">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label class="form-check-label" for="remember">
+                                {{ __('Lembrar') }}
+                            </label>
                         </div>
                     </div>
+                </div>
 
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label">{{ __('Password') }}</label>
-                        <div>
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                <div class="form-group row">
+                    <div class="authButtonsContainer">
+                        <a href="/register">
+                            <button type="button" class="authButtons">{{ __('Registar') }}</button>   
+                        </a>
 
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                        <button type="submit" id="loginButton" class="authButtons">{{ __('Entrar') }}</button>         
                     </div>
+                </div> 
 
-                    <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                <label class="form-check-label" for="remember">
-                                    {{ __('Lembrar') }}
-                                </label>
-                            </div>
-                        </div>
+                <div class="form-group row">
+                    <div id="authLinkContainer">
+                        @if (Route::has('password.request'))  
+                            <a class="authLink" href="{{ route('password.request') }}">{{ __('Esqueceu-se da password?') }}</a>
+                        @endif
                     </div>
+                </div> 
 
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Entrar') }}
-                            </button>       
-                        </div>
-                        <div>
-                            <ul class="navbar-nav ms-auto">
-                                @if (Route::has('register'))
-                                    <li class="dropdown-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Registar') }}</a>
-                                    </li>
-                                @endif
-                                @if (Route::has('password.request'))
-                                    <li class="dropdown-item">
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">{{ __('Esqueceu-se da password?') }}</a>
-                                    </li>
-                                @endif
-                            </ul>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            </form>
         </div>
     </div>
 </div>
 @endsection
+
+
+
+
