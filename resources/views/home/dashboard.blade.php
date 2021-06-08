6317c3ca275730c194fb9a100f7fa91162da87e5
Afonsommlourenco <83539918+Afonsommlourenco@users.noreply.github.com>
2021-06-08 20:58:47 +0100

    f

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -1,16 +1,22 @@
 @extends('template')
 @section('content')
+<style>
+.mainmenu{
+    position: fixed;
+    width:100%;
+    margin-bottom: 80px;
+}
+</style>
+<div >
+
+</div >
 <div class="adminMenu">
 
 <a href="{{route('Dashboard')}}">Dashboard</a>
 <a href="{{route('Orders')}}">Encomendas</a>
-<<<<<<< Updated upstream
 <a href="{{route('Users')}}">Utilizadores</a>
-<a href="{{route('Stamps')}}">Estampas</a>
-=======
-<a href="">Utilizadores</a>
-<a href="#">Estampas</a>
->>>>>>> Stashed changes
+
+
 <a href="#">Cores</a>
 </div>
 <div class="adminContent">
