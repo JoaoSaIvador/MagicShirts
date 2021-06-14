929390ce4ecc247b95ae26674519fad6eef69d04
João Salvador <jpvsalvador05@gmail.com>
2021-06-14 19:10:48 +0100

    Auto stash before merge of "main" and "origin/main"

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Estampa;
 use Illuminate\Http\Request;
 use App\Models\Cor;
+use App\Models\Preco;
 use App\Http\Requests\ProductPost;
 
 class CartController extends Controller
@@ -33,6 +34,8 @@ class CartController extends Controller
         $request->validated();
         $carrinho = $request->session()->get('carrinho', []);
         $key = "$request->estampa_id" . '-' . "$request->cor_codigo" . '-' . "$request->tamanho";
+        $preco = Preco::find(1);
+        $estampa = Estampa::find($request->estampa_id);
 
         if (empty($carrinho)) {
             $carrinho['precoTotal'] = 0;
@@ -41,12 +44,40 @@ class CartController extends Controller
         else {
             if(array_key_exists($key, $carrinho['items'])) {
                 $quantidade = $carrinho['items'][$key]['quantidade'] + $request->quantidade;
-                $request->session()->put("carrinho.items.$key.quantidade", $quantidade);
+                $carrinho['items'][$key]['quantidade'] = $quantidade;
 
+                if($quantidade >= $preco['quantidade_desconto']) {
+                    $oldSubtotal = $carrinho['items'][$key]['subtotal'];    
+
+                    if($estampa['cliente_id'] != null) {
+                        $precoTshirt = $preco['preco_un_proprio_desconto'];
+                    } else {
+                        $precoTshirt = $preco['preco_un_catalogo_desconto'];
+                    }
+
+                    $newSubtotal = $precoTshirt * $quantidade;
+
+                    $carrinho['items'][$key]['preco_un'] = $precoTshirt;
+                    $carrinho['items'][$key]['subtotal'] = $newSubtotal;
+                    $carrinho['precoTotal'] -= $oldSubtotal;
+                    $carrinho['precoTotal'] += $newSubtotal;
+                }
+
+                $request->session()->put('carrinho', $carrinho);
                 return back()
-                ->with('alert-msg', 'Foi adicionada uma tshirt carrinho!')
+                ->with('alert-msg', 'Foi adicionada uma tshirt ao carrinho!')
                 ->with('alert-type', 'success');
             }
+        }
+
+        if ($request->quantidade >= $preco['quantidade_desconto']) {
+            if($estampa['cliente_id'] != null) {
+                $precoTshirt = $preco['preco_un_proprio_desconto'];
+            } else {
+                $precoTshirt = $preco['preco_un_catalogo_desconto'];
+            }
+        } else {
+            $precoTshirt = $request->preco_un;
         }
 
         $carrinho['items']["$request->estampa_id" . '-' . "$request->cor_codigo" . '-' . "$request->tamanho"] = [
@@ -54,18 +85,18 @@ class CartController extends Controller
             'estampa_id' => $request->estampa_id,
             'cor_codigo' => $request->cor_codigo,
             'tamanho' => $request->tamanho,
-            'preco' => $request->preco,
-            'subtotal' => ($request->preco * $request->quantidade),
+            'preco_un' => $precoTshirt,
+            'subtotal' => ($precoTshirt * $request->quantidade),
             'nome' => Estampa::where('id', $request->estampa_id)->value('nome'),
             'imagem' => Estampa::where('id', $request->estampa_id)->value('imagem_url')
         ];
 
-        $carrinho['precoTotal'] += $request->preco * $request->quantidade;
+        $carrinho['precoTotal'] += $precoTshirt * $request->quantidade;
         $carrinho['quantidadeItens'] += 1;
 
         $request->session()->put('carrinho', $carrinho);
         return back()
-            ->with('alert-msg', 'Foi adicionada uma tshirt carrinho!')
+            ->with('alert-msg', 'Foi adicionada uma tshirt ao carrinho!')
             ->with('alert-type', 'success');
     }
 
@@ -74,6 +105,8 @@ class CartController extends Controller
         $carrinho = $request->session()->get('carrinho', []);
         $quantidade = $request->quantidade;
         $newIndex = $carrinho['items'][$index]['estampa_id'] . '-' . $request->cor_codigo . '-' . $request->$index;
+        $preco = Preco::find(1);
+        $estampa = Estampa::find($request->estampa_id);
 
         if ($quantidade <= 0) {
             $carrinho['quantidadeItens']--;
@@ -86,13 +119,32 @@ class CartController extends Controller
             if(array_key_exists($newIndex, $carrinho['items']) && $index != $newIndex) {
                 $quantidade = $carrinho['items'][$newIndex]['quantidade'] + $request->quantidade;
                 $carrinho['items'][$newIndex]['quantidade'] = $quantidade;
-                $carrinho['items'][$newIndex]['subtotal'] = ($carrinho['items'][$newIndex]['preco'] * $quantidade);
+
+                if($quantidade >= $preco['quantidade_desconto']) {
+                    $oldSubtotal = $carrinho['items'][$newIndex]['subtotal'];    
+
+                    if($estampa['cliente_id'] != null) {
+                        $precoTshirt = $preco['preco_un_proprio_desconto'];
+                    } else {
+                        $precoTshirt = $preco['preco_un_catalogo_desconto'];
+                    }
+
+                    $newSubtotal = $precoTshirt * $quantidade;
+
+                    $carrinho['items'][$newIndex]['preco_un'] = $precoTshirt;
+                    $carrinho['items'][$newIndex]['subtotal'] = $newSubtotal;
+                    $carrinho['precoTotal'] -= $oldSubtotal;
+                    $carrinho['precoTotal'] += $newSubtotal;
+                } else {
+                    $carrinho['items'][$newIndex]['subtotal'] = ($carrinho['items'][$newIndex]['preco_un'] * $quantidade);
+                }
+
                 $carrinho['quantidadeItens']--;
                 unset($carrinho['items'][$index]);
                 $request->session()->put('carrinho', $carrinho);
 
                 return back()
-                ->with('alert-msg', 'Foi adicionada uma tshirt carrinho!')
+                ->with('alert-msg', 'Foi adicionada uma tshirt ao carrinho!')
                 ->with('alert-type', 'success');
             }
 
@@ -126,7 +178,7 @@ class CartController extends Controller
             $request->session()->put('carrinho', $carrinho);
 
             return back()
-                ->with('alert-msg', 'A T-shirt foi removida')
+                ->with('alert-msg', 'A T-shirt foi removida do carrinho')
                 ->with('alert-type', 'success');
         }
         return back()
